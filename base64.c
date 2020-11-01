537cd3a661ec9dc805a7c09a596addeadfa1ce4a
Jérôme Béhé <jerome.behe@gmail.com>
2020-11-01 21:46:41 +0100

    Add argument parser

--- base64.c
+++ base64.c
@@ -1,5 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <argp.h>
+
+const char * argp_program_version = "0.0.1";
+static char doc[] = "base64 encoding and decoding utility program";
+static struct argp_option options[] = {
+  {"encode", 'e', 0, 0, "Encode input in base64", 0},
+  {"decode", 'd', 0, 0, "Decode input from base64", 0},
+};
+
+struct arguments
+{
+  int encode;
+  int decode;
+};
+
+/* Parse a single option. */
+static error_t
+parse_opt(int key, char *arg, struct argp_state *state)
+{
+  /* Get the input argument from argp_parse, which we
+     know is a pointer to our arguments structure. */
+  struct arguments *arguments = state->input;
+
+  switch (key)
+    {
+    case 'e':
+      arguments->encode = 1;
+      break;
+    case 'd':
+      arguments->decode = 1;
+      break;
+    case ARGP_KEY_SUCCESS:
+      if ((arguments->encode == 1) && (arguments->decode == 1))
+        argp_error(state, "Encode and decode options shall not be used simultaneously");
+      else if ((arguments->encode == 0) && (arguments->decode == 0))
+        argp_error(state, "Please specify one of the options encode or decode");
+    default:
+      return ARGP_ERR_UNKNOWN;
+    }
+  return 0;
+}
+
+
+static struct argp argp = {options, parse_opt, 0, doc, 0, 0, 0 };
 
 
 const char dic[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
@@ -10,22 +54,32 @@ const char dic[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
 
 void encode(char* src, char* dst);
 void decode(char* src, char* dst);
-
 int convertChar(char in, char* out);
 
-int main(int argc, char const *argv[]) {
-  char inBuf[100], outBuf[200] = {0};
-  sprintf(inBuf, "tester");
+// MAIN
+int main(int argc, char *argv[]) {
+  struct arguments arguments;
+  arguments.encode = 0;
+  arguments.decode = 0;
 
-  encode(inBuf, outBuf);
+  argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
-  printf("%s\n%s\n", inBuf, outBuf);
+char inBuf[100], outBuf[200] = {0};
 
 
-  sprintf(inBuf, "Q2xlbWVudA==");
-  decode(inBuf, outBuf);
-  printf("%s\n%s\n", inBuf, outBuf);
+  if (arguments.encode)
+  {
+    sprintf(inBuf, "tester");
+    encode(inBuf, outBuf);
+    printf("%s\n%s\n", inBuf, outBuf);
+  }
 
+  if (arguments.decode)
+  {
+    sprintf(inBuf, "Q2xlbWVudA==");
+    decode(inBuf, outBuf);
+    printf("%s\n%s\n", inBuf, outBuf);
+  }
 
 
   return 0;
