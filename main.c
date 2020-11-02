b1dd702fccb3af1ae033f7a263ce041ce225356a
Jérôme Béhé <jerome.behe@gmail.com>
2020-11-02 21:17:38 +0100

    Restore useless parts
    This reverts commit f413622baa29df7efab90a96197ac59a78188436.

--- main.c
+++ main.c
@@ -6,12 +6,18 @@
 const char * argp_program_version = "0.0.1";
 static char doc[] = "base64 encoding and decoding utility program";
 static struct argp_option options[] = {
+//  {"verbose", 'v', 0, 0, "Produce verbose output\0", 0},
+//  {"in", 'i', "FILE", 0, "Produce verbose output\0", 0},
+//  {"verbose", 'v', 0, 0, "Produce verbose output\0", 0},
   {"encode", 'e', 0, 0, "Encode input in base64", 0},
   {"decode", 'd', 0, 0, "Decode input from base64", 0},
+//   {"input", 'i', 0, 0, "Input file"}
 };
 
 struct arguments
 {
+//  char *args[2];
+//  int verbose;
   int encode;
   int decode;
 };
@@ -35,6 +41,27 @@ parse_opt(int key, char *arg, struct argp_state *state)
     case 'd':
       arguments->decode = 1;
       break;
+    // case 'v':
+    //   arguments->verbose = 1;
+    //   break;
+    // case 'o':
+    //   arguments->output_file = arg;
+    //   break;
+
+    // case ARGP_KEY_ARG:
+    //   if (state->arg_num >= 2)
+    //     /* Too many arguments. */
+    //     argp_usage (state);
+    //
+    //   arguments->args[state->arg_num] = arg;
+    //
+    //   break;
+    //
+    // case ARGP_KEY_END:
+    //   if (state->arg_num < 2)
+    //     /* Not enough arguments. */
+    //     argp_usage (state);
+    //   break;
     case ARGP_KEY_SUCCESS:
       if ((arguments->encode == 1) && (arguments->decode == 1))
         argp_error(state, "Encode and decode options shall not be used simultaneously");
